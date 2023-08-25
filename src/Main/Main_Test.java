2b7789d57b3e7ed0592e650e923b8b9d2a8fa13c
HP <osmanenosman@hotmail.com>
2023-08-25 09:40:12 +0200

    US 3(Logout olma) eklendi

--- src/Main/Main_Test.java
+++ src/Main/Main_Test.java
@@ -5,6 +5,7 @@ import Utilty.MyFunc;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
@@ -46,6 +47,48 @@ public class Main_Test extends BaseDriver {
         driver = new ChromeDriver();
 
         //Codu bu alana yazabiliriz
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.akakce.com/");
+        MyFunc.Bekle(2);
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        MyFunc.Bekle(2);
+
+        WebElement girisYap = driver.findElement(By.xpath("(//a[text()='Giriş Yap'])[1]"));
+        girisYap.click();
+        MyFunc.Bekle(2);
+
+        WebElement inputEmail = driver.findElement(By.xpath("//input[@id='life']"));
+        inputEmail.sendKeys("grup16basarir@gmail.com");
+        MyFunc.Bekle(2);
+
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@id='lifp']"));
+        inputPassword.sendKeys("1234567Ss");
+        MyFunc.Bekle(2);
+
+        WebElement girisYapTikla = driver.findElement(By.xpath("(//input[@type='submit'])[2]"));
+        girisYapTikla.click();
+        MyFunc.Bekle(2);
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        MyFunc.Bekle(2);
+
+        WebElement hesapAd = driver.findElement(By.xpath("(//a[@rel='nofollow'])[1]"));
+        Actions aksiyonlar = new Actions(driver);
+        Action aksiyon = aksiyonlar.moveToElement(hesapAd).build();
+        aksiyon.perform();
+        MyFunc.Bekle(2);
+
+        WebElement logOut = driver.findElement(By.xpath("//a[text()='Çık']"));
+        logOut.click();
+        MyFunc.Bekle(2);
+
+        WebElement dogrulama = driver.findElement(By.xpath("(//div[contains(@id, 'v8')])[1]"));
+        Assert.assertTrue(dogrulama.getText().contains("Giriş Yap"));
 
         BekleVeKapat();
     }
